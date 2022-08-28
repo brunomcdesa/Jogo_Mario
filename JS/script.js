e598520c485b22c48d582d38d0f60559694b08d6
brunomcdesa <brunotkd68@gmail.com>
2022-08-28 11:28:49 -0300

    Adição do botão par reiniciar o jogo

--- JS/script.js
+++ JS/script.js
@@ -5,7 +5,19 @@ const pontuacao = document.querySelector('.pontuacao')
 var maiorPontuacao = document.querySelector('.pontuacaomaior'); 
 var pontuacaomaxima = 0;
 
+// Funcao para criar botao para recomeçar o jogo
+function botao(){
+    var btn = document.createElement("button");
+    var lbl = document.createTextNode("Recomeçar");
+    btn.appendChild(lbl);
+    btn.classList.add('botao')
+    btn.addEventListener("click", function(){
+        location.reload();
+   })
+    document.body.appendChild(btn);
+}
 
+//Funcao para efetuar o pulo
 const pulo = () => {
     mario.classList.add('jump');
 
@@ -14,6 +26,8 @@ const pulo = () => {
     }, 500);
 }
 
+
+//funcao principal do jogo
 const loop = setInterval(() => {
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
@@ -22,25 +36,36 @@ const loop = setInterval(() => {
 
    //if para encerramento
     if(pipePosition <= 145 && pipePosition > 0 && marioPosition < 89){
+       // encerrar animação do tubo
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
 
+        //encear anmação do mario
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPosition}px`;
         mario.src = './IMAGENS/game-over.png';
         mario.style.width = '90px';
         mario.style.marginLeft = '50px';
 
+        //encerra animação da nuvem
         clouds.style.animation ='none';
         clouds.style.left = `${cloudsPosition}px`;
 
         clearInterval(loop);
+        botao.apply();
+
     }
     else{
-        let ultimaPontuacao = 0
+        
         pontuacaomaxima = pontuacaomaxima + 1;
         pontuacao.innerHTML = "Pontução: " + pontuacaomaxima;
         console.log(pontuacaomaxima);
+
+            // IF PARA DIMINUIR O TEMPO DA ANIMACAO DEPOS DE CERTA PONTUACAO
+        if (pontuacaomaxima > 100){
+            pipe.style.animation
+            console.log(pipe.style.animation);
+        }
      //   if(ultimaPontuacao < pontuacaomaxima){
             
      //       ultimaPontuacao = pontuacaomaxima;
